9a20798a1ebef28d5ce2b739389f403798052559
Ana Clara Mendes Bull <89466535+anaclarabull@users.noreply.github.com>
2021-11-16 20:18:17 -0300

    crud

--- src/main/java/org/serratec/backend/projetoFinal/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package org.serratec.backend.projetoFinal.repository;
-
-import org.serratec.backend.projetoFinal.domain.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
